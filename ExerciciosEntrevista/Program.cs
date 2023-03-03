66d666e94ef48108a02b2e2ad583446653a32b2c
Marcos Andrade <mvpdeandrade@hotmail.com>
2023-03-03 13:53:37 -0300

    Adicionar arquivos de projeto.

---
+++ ExerciciosEntrevista/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ExerciciosEntrevista
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int indice = 13;
+            int soma = 0;
+            int k = 0;
+            while (k < indice)
+            {
+                k = k + 1;
+                soma = k + k;
+            }
+            Console.WriteLine(soma);
+        }
+    }
+}
